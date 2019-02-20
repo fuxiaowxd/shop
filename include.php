7c15736ddab5bd31f057d5ea3d221cbfc0011c78
fuxiaowxd <39288970+fuxiaowxd@users.noreply.github.com>
2019-02-20 08:54:58 +0800

    Add files via upload

---
+++ include.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *Date:2019/2/19
+ *Time:16:11
+ */
+
+header('Content-type:text/html;Charset=utf-8');
+date_default_timezone_set('PRC');
+
+session_start();
+
+require_once './lib/commonFunc.php';
+require_once './lib/stringFunc.php';
+require_once './lib/imageFunc.php';
